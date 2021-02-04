704576fdd366cbec6eba9262d30b655ee2373fa5
decongo <conger.devon@gmail.com>
2021-02-03 22:06:32 -0700

    added interactions with blockchain

---
+++ src/store/interactions.js
@@ -0,0 +1,44 @@
+import Web3 from 'web3';
+import Dimu from '../abis/DistributedMusic.json';
+import {
+  web3Loaded,
+  dimuLoaded,
+  trackAdded,
+  accountLoaded
+} from './actions.js';
+
+export const loadWeb3 = (dispatch) => {
+  const web3 = new Web3(Web3.givenProvider || 'http://localhost:7575');
+  dispatch(web3Loaded(web3));
+  return web3;
+}
+
+export const loadDimu = async (web3, networkID, dispatch) => {
+  try {
+    const dimuReference = new web3.eth.Contract(Dimu.abi, Dimu.networks[networkID].address);
+    dispatch(dimuLoaded(dimuReference));
+    return dimuReference;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+export const loadAccount = async (web3, dispatch) => {
+  const accounts = await web3.eth.getAccounts();
+  const account = accounts[0];
+  dispatch(accountLoaded(account));
+  return account;
+}
+
+export const uploadTrack = async (dimu, track, account, dispatch) => {
+  dimu.methods.uploadTrack('testhash', track.title).send({ from: account })
+  .on('transactionHash', hash => {
+    dispatch(trackAdded(track));
+  })
+  .on('error', error => {
+    console.error(error);
+    window.alert('There was an error.');
+  });
+}
+
